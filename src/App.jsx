cfbce343be857e0fc0648866ba18ab39d0f9f147
kyeol1202 <kyeol9927@gmail.com>
2026-04-01 16:20:21 +0900

    chore: CRA에서 Vite로 마이그레이션

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from './components/Navbar.jsx';
+import Hero from './components/Hero.jsx';
+import About from './components/About.jsx';
+import Skills from './components/Skills.jsx';
+import Projects from './components/Projects.jsx';
+import Contact from './components/Contact.jsx';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
